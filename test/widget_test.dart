50228e92efaa0c89404c1ddb13ce862be11c4059
saika <you@example.com>
2021-03-13 17:35:30 +0800

    add lowercase to test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Tweet/main.dart';
+import 'package:tweet/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
